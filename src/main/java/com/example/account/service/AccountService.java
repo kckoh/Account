de3cd3ead31ebeaf33afd609b2d551a75476bfbc
kckoh <kyungcheolkoh@gmail.com>
2022-08-05 16:55:19 -0700

    account/delete implement

--- src/main/java/com/example/account/service/AccountService.java
+++ src/main/java/com/example/account/service/AccountService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -21,35 +22,24 @@ public class AccountService {
     private final AccountUserRepository accountUserRepository;
 
     /**
-     *
-     search users
-     create account number
-     save the account and pass the number
+     * search users
+     * create account number
+     * save the account and pass the number
      */
     @Transactional
     public AccountDto createAccount(Long userId, Long initialBalance) {
-        AccountUser accountUser = accountUserRepository.findById(userId)
-                .orElseThrow(() -> new AccountException(ErrorCode.USER_NOT_FOUND));
+        AccountUser accountUser = accountUserRepository.findById(userId).orElseThrow(() -> new AccountException(ErrorCode.USER_NOT_FOUND));
 
         validateCreateAccount(accountUser);
 
-        String newAccountNumber = accountRepository.findFirstByOrderByIdDesc()
-                .map(account -> (Integer.parseInt(account.getAccountNumber())) + 1 + "")
-                .orElse("1000000000");
+        String newAccountNumber = accountRepository.findFirstByOrderByIdDesc().map(account -> (Integer.parseInt(account.getAccountNumber())) + 1 + "").orElse("1000000000");
 
-        return AccountDto.fromEntity(accountRepository.save(Account.builder()
-                .accountUser(accountUser)
-                .accountStatus(AccountStatus.IN_USE)
-                .accountNumber(newAccountNumber)
-                .balance(initialBalance)
-                .registeredAt(LocalDateTime.now())
-                .build()
-        ));
+        return AccountDto.fromEntity(accountRepository.save(Account.builder().accountUser(accountUser).accountStatus(AccountStatus.IN_USE).accountNumber(newAccountNumber).balance(initialBalance).registeredAt(LocalDateTime.now()).build()));
 
     }
 
     private void validateCreateAccount(AccountUser accountUser) {
-        if (accountRepository.countByAccountUser(accountUser) ==10) {
+        if (accountRepository.countByAccountUser(accountUser) == 10) {
             throw new AccountException(ErrorCode.MAX_ACCOUNT_PER_USER_10);
         }
     }
@@ -61,4 +51,32 @@ public class AccountService {
         }
         return accountRepository.findById(id).get();
     }
+
+    @Transactional
+    public AccountDto deleteAccount(Long userId, String accountNumber) {
+        AccountUser accountUser = accountUserRepository.findById(userId).orElseThrow(() -> new AccountException(ErrorCode.USER_NOT_FOUND));
+        Account account = accountRepository.findByAccountNumber(accountNumber).orElseThrow(() -> new AccountException(ErrorCode.ACCOUNT_NOT_FOUND));
+
+        validateDeleteAccount(accountUser, account);
+
+        account.setAccountStatus(AccountStatus.UNREGISTERED);
+        account.setUnRegisteredAt(LocalDateTime.now());
+        return AccountDto.fromEntity(account);
+    }
+
+    private void validateDeleteAccount(AccountUser accountUser, Account account) {
+        if (!Objects.equals(accountUser.getId(), account.getAccountUser().getId())) {
+            throw new AccountException(ErrorCode.USER_ACCOUNT_UNMATCH);
+        }
+        if (account.getAccountStatus() == AccountStatus.UNREGISTERED) {
+            throw new AccountException(ErrorCode.ACCOUNT_ALREDAY_REGISTERED);
+        }
+        if (account.getBalance() > 0) {
+            throw new AccountException(ErrorCode.BALANCE_NOT_EMPTY);
+
+        }
+
+    }
+
+
 }
